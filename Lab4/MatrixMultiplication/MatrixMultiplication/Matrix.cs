9891ef0d4c6c2fa02f3466e5ebbfe652f8fad33b
Alexandra <alexis07oana@yahoo.com>
2019-11-17 13:59:47 +0200

    Lab 4

---
+++ Lab4/MatrixMultiplication/MatrixMultiplication/Matrix.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatrixMultiplication
+{
+    public class Matrix
+    {
+        private int _row;
+        private int _column;
+        private int[] _listElements;
+
+        public Matrix(int r, int c)
+        {
+            _row = r;
+            _column = c;
+            _listElements = new int[_row * _column];
+        }
+
+        public int Row { get => _row; set => _row = value; }
+        public int Column { get => _column; set => _column = value; }
+        public int[] ListElements { get => _listElements; set => _listElements = value; }
+
+        public List<Tuple<int, int>> GetElementsBetweenIndexes(int a, int b)
+        {
+            List<Tuple<int, int>> _elements = new List<Tuple<int, int>>();
+            for (int i = a; i < b; i++)
+                _elements.Add(new Tuple<int, int>(i / Column, i % Column));
+            return _elements;
+        }
+        public string GetElementsToString()
+        {
+            string x = "";
+            foreach (int el in ListElements)
+                x += el + " ";
+            return x;
+        }
+        public override string ToString()
+        {
+            return "Matrix: " + GetElementsToString();
+        }
+
+        public int GetByPositions(int i, int j)
+        {
+            return ListElements[Column * i + j];
+        }
+
+        public void PutByPositions(int i, int j, int value)
+        {
+            var pos = Column * i + j;
+            ListElements[pos] = value;
+        }
+    }
+}
---
+++ Lab4/MatrixMultiplication/MatrixMultiplication/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MatrixMultiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix _first = new Matrix(2, 2);
+            Matrix _second = new Matrix(2, 3);
+            Matrix _third = new Matrix(3, 2);
+
+            Console.Write("Enter number of threads: ");
+            int _nrThreads = Convert.ToInt32(Console.ReadLine());
+
+            var _matrixOpersations = new MatrixOperations(_first, _second, _third, _nrThreads);
+            _matrixOpersations.PopulateMatrices();
+
+            var _intermediateMatrixSplitByThreads = _matrixOpersations.ThreadElements(_matrixOpersations.Intermediar);
+            var _resultMatrixSplitByThreads = _matrixOpersations.ThreadElements(_matrixOpersations.Result);
+
+            //int _matrixElementCount = _resultMatrixSplitByThreads.Keys.Count;
+
+            var _intermediarThreads = new List<Task>();
+            var _resultThreads = new List<Task>();
+            foreach (int k in _intermediateMatrixSplitByThreads.Keys)
+            {
+                _intermediarThreads.Add(new Task(() => { _matrixOpersations.MultiplyMatrices(_intermediateMatrixSplitByThreads[k]); }));
+            }
+            foreach (int k in _resultMatrixSplitByThreads.Keys)
+            {
+                _resultThreads.Add(new Task(() => { _matrixOpersations.MultipyFinalMatrix(_resultMatrixSplitByThreads[k], _matrixOpersations.Result); }));
+            }
+
+            foreach (var t in _intermediarThreads)
+                t.Start();
+            foreach (var t in _resultThreads)
+                t.Start();
+
+
+
+
+            //Console.WriteLine(_first.ToString());
+            //Console.WriteLine(_second.ToString());
+            //Console.WriteLine(_matrixOpersations.IntermediarTaskList.ToString());
+            Console.ReadLine();
+        }
+    }
+}
